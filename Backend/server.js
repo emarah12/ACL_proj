60bbf0b597791304cf225cc40f3a07b0001b52a7
emarah12 <emarah.mohamed@gmail.com>
2022-01-15 01:35:20 +0100

    lobna

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,41 @@
 // call the packages we need
 const express = require('express');
 const app = express();
+const bcrypt= require('bcrypt')
+app.use(express.json())
+const users = []
+app.get('/users',(req,res) => {
+    res.json(users)
+})
+app.post('/users',async(req,res)=>{
+    try{   
+        const hashedPassword=await bcrypt.hash(req.body.password,10)
+        const user=  {name: req.body.name, password:hashedPassword}
+            users.push(user)
+            res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+ 
+   
+
+})
+app.post('/users/login',async(req,res)=>{
+const user = users.find(user =>user.name= req.body.name)
+if (user==null){
+    return res.status(400).send('Cannot find user')
+}
+try {
+if(await bcrypt.compare(req.body.password,user.password))
+res.send('Success')
+} else {
+    res.send('Not Allowed')
+}
+catch {
+    res.status(500).send()
+}
+})
+app.listen(3000)
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var moment = require('moment'); // require
@@ -39,6 +74,7 @@ var Flight = require('./Models/Flight');
 // import { adminController } from "./Backend/Controllers/AdminController"
 //import { flightController } from "./Backend/Controllers/FlightController"
 var flightController = require('./Controllers/FlightController');
+const { use } = require('./Controllers/FlightController');
 // ROUTES FOR OUR API
 // =============================================================================
 
